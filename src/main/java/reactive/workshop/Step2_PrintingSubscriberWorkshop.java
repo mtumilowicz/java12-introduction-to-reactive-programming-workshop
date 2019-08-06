078d1f50c9d7cba2e07b4c9f5b5ae1bc9a6f7d02
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-08-06 19:37:23 +0200

    Step2_PrintingSubscriberWorkshop.java

---
+++ src/main/java/reactive/workshop/Step2_PrintingSubscriberWorkshop.java
@@ -0,0 +1,13 @@
+package reactive.workshop;
+
+/**
+ * Created by mtumilowicz on 2018-05-21.
+ */
+class Step2_PrintingSubscriberWorkshop<T> extends Step1_SubscriberBaseWorkshop<T> {
+
+    @Override
+    public void onNext(T item) {
+        System.out.println("onNext, item: " + item);
+        // request next 1 elem, hint: subscription.request(...)
+    }
+}
