ca13b911273bc189d2d092deda3f793f439a4e96
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-08-06 20:00:37 +0200

    Step4_NumberPublisherWorkshop.java

--- src/main/java/reactive/workshop/Step4_NumberPublisherWorkshop.java
+++ src/main/java/reactive/workshop/Step4_NumberPublisherWorkshop.java
@@ -11,10 +11,6 @@ class Step4_NumberPublisherWorkshop extends SubmissionPublisher<Integer> {
     void start() {
         // generate infinite stream of numbers, submit and sleep for each number
         // hint: IntStream, iterate, submit, sleep
-        IntStream.iterate(1, i -> i + 1).forEach(i -> {
-            submit(i);
-            sleep();
-        });
     }
 
     private void sleep() {
