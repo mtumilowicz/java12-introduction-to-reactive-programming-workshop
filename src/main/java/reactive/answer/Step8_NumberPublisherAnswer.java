2f9562240aa26c7280729281d458f0eaa89094e9
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-01-15 22:45:57 +0100

    answers: reusing abstractions

--- src/main/java/reactive/answer/Step8_NumberPublisherAnswer.java
+++ src/main/java/reactive/answer/Step8_NumberPublisherAnswer.java
@@ -1,23 +1,5 @@
 package reactive.answer;
 
-import java.util.concurrent.SubmissionPublisher;
-import java.util.stream.IntStream;
-
-class Step8_NumberPublisherAnswer extends SubmissionPublisher<Integer>
+class Step8_NumberPublisherAnswer extends Step3_NumberPublisherAnswer
         implements Step7_StreamAnswer<Integer> {
-
-    void start() {
-        IntStream.iterate(1, i -> i + 1).forEach(i -> {
-            submit(i);
-            sleep();
-        });
-    }
-
-    private void sleep() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
