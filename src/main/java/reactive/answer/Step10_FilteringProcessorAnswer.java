4107ff644fb57763fea5a7b09fa1652c99230e5a
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-01-15 22:44:14 +0100

    answers: reusing abstractions

--- src/main/java/reactive/answer/Step10_FilteringProcessorAnswer.java
+++ src/main/java/reactive/answer/Step10_FilteringProcessorAnswer.java
@@ -2,19 +2,9 @@ package reactive.answer;
 
 import java.util.function.Predicate;
 
-class Step10_FilteringProcessorAnswer<T> extends Step4_ProcessorBaseAnswer<T, T> implements Step7_StreamAnswer<T> {
-    private final Predicate<T> condition;
+class Step10_FilteringProcessorAnswer<T> extends Step6_FilteringProcessorAnswer<T> implements Step7_StreamAnswer<T> {
 
     Step10_FilteringProcessorAnswer(Predicate<T> condition) {
-        this.condition = condition;
-    }
-
-    @Override
-    public void onNext(T item) {
-        if (condition.test(item)) {
-            submit(item);
-        }
-
-        subscription.request(1);
+        super(condition);
     }
 }
--- src/main/java/reactive/answer/Step9_MappingProcessorAnswer.java
+++ src/main/java/reactive/answer/Step9_MappingProcessorAnswer.java
@@ -2,17 +2,9 @@ package reactive.answer;
 
 import java.util.function.Function;
 
-class Step9_MappingProcessorAnswer<In, Out> extends Step4_ProcessorBaseAnswer<In, Out>
+class Step9_MappingProcessorAnswer<In, Out> extends Step5_MappingProcessorAnswer<In, Out>
         implements Step7_StreamAnswer<Out> {
-    private final Function<In, Out> mapper;
-
     Step9_MappingProcessorAnswer(Function<In, Out> mapper) {
-        this.mapper = mapper;
-    }
-
-    @Override
-    public void onNext(In item) {
-        submit(mapper.apply(item));
-        subscription.request(1);
+        super(mapper);
     }
 }
